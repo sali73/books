1a39d7ed48e2d4abdf7fe0ee0d869746dc813c32
sali73 <sally.elgendy@hotmail.com>
2020-09-24 12:34:53 -0400

    fix routs

--- controllers/books.js
+++ controllers/books.js
@@ -5,50 +5,53 @@ const Books = require('../models/books.js');
 /////////////////// 
 //Index Route
 //////////////////
+//==| CREATE |
 router.post('/', async (req, res) => {
-   try {
-       const createdBook = await Books.create(req.body);
-       res.status(200).json(createdBook);
-   } catch (error) {
-       res.status(400).json(error);
-   }
+    try {
+        const createdBookmark = await Books.create(req.body);
+        Books.find({}, (err, docs) => {console.log(docs)})
+        res.status(200).json(createdBookmark);
+    }
+    catch(error) {
+        console.log(error);
+        res.status(400).json(error);
+    }
 });
+
+//==| INDEX |
 router.get('/', async (req, res) => {
-   try {
-       const gitbooks = await Books.find({});
-       res.status(200).json(gitbooks);
-   } catch (error) {
-       res.status(400).json(error);
-   }
+    try {
+        const bookmarks = await Books.find({})
+        res.status(200).json(bookmarks)
+    }
+    catch (error) {
+        res.status(400).json(error);
+    }
 });
- 
-///////////////////
-//Delete Route
-///////////////////
+
+//==| DELETE |
 router.delete('/:id', async (req, res) => {
-   try {
-       const deletedBooks = await Books.findByIdAndRemove(req.params.id);
-       res.status(200).json(deletedBooks);
-   } catch (error) {
-       res.status(400).json(error);
-   }
+    try {
+        const deletedbookmark = await Books.findByIdAndDelete(req.params.id);
+        res.status(200).json(deletedbookmark);
+    } catch (error) {
+        res.status(400).json(error);
+    }
 });
- 
- ///////////////////
-//Edit Route
-///////////////////
-router.put('/:id', async (req, res) => {
-   try {
-       const updatedBooks = await Books.findByIdAndUpdate(
-           req.params.id,
-           req.body,
-           { new: true }
-       );
-       res.status(200).json(updatedBooks);
-   } catch (error) {
-       res.status(400).json(error);
-   }
+
+//==| UPDATE |
+router.put('/:id',  async (req, res) => {
+    try {
+        const updatedBookmark= await Books.findByIdAndUpdate(
+            req.params.id,
+            req.body
+        );
+        res.status(200).json(updatedBookmark);
+    } catch (error) {
+        res.status(400).json(error);
+    }
 });
- 
- 
+//=============================================================================| ROUTES END |
+
+//==| EXPORT ROUTER |
 module.exports = router;
